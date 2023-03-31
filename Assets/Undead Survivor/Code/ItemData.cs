2355809be16ebac480c57b9d63474821034f4bdc
CreatorPP <86832002+CreatorPP@users.noreply.github.com>
2023-04-01 08:30:31 +0900

    Item Data 생성

---
+++ Assets/Undead Survivor/Code/ItemData.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Item", menuName ="Scriptble Object/ItemData")]
+public class ItemData : ScriptableObject
+{
+
+    public enum ItemType {  Mell, Range, Glove, Shoe, Heal} // 근접 원거리 글러브 신발 체력
+
+
+    [Header("# Main Info")]
+    public ItemType itemType;
+    public int itemId;
+    public string itemName;
+    public string itemDesc;
+    public Sprite itemIcon;
+
+
+    [Header("# Level Data")]
+    public float baseDamage;
+    public int baseCount;
+    public float[] damages;
+    public int[] counts;
+
+    [Header("# Weapon")]
+    public GameObject projectile;
+
+}
