0ced0485a54f6a77684f076c3d6a1d620b05c6cc
CreatorPP <86832002+CreatorPP@users.noreply.github.com>
2023-04-12 08:55:58 +0900

    캐릭터 업적 해금 구현
    캐릭터 업적 해금 스크립트 작성

--- Assets/Undead Survivor/Code/AchiveManager.cs
+++ Assets/Undead Survivor/Code/AchiveManager.cs
@@ -3,21 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+
+//업적 관리 코드
 public class AchiveManager : MonoBehaviour
 {
 
+  
+
     public GameObject[] lockCharacter;
     public GameObject[] unlockCharacter;
+    public GameObject uiNotice;
 
     enum Achive { UnlockPotato, UnlockBean }
     Achive[] achives;
 
-    private void Awake()
+    WaitForSecondsRealtime wait;
+
+    void Awake()
     {
         achives = (Achive[])Enum.GetValues(typeof(Achive));
+        wait = new WaitForSecondsRealtime(4);
 
-
-        if(!PlayerPrefs.HasKey("MyData")) // MyData가 없으면 초기화 해달라
+        if (!PlayerPrefs.HasKey("MyData")) // MyData가 없으면 초기화 해달라
         {
             Init();
         }
@@ -37,7 +44,7 @@ public class AchiveManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        UnlockCharacter();
     }
 
     void UnlockCharacter()
@@ -53,8 +60,49 @@ public class AchiveManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        foreach(Achive achive in achives)
+        {
+            CheckAchive(achive);
+        }
+    }
+
+    void CheckAchive(Achive achive) //업적달성 함수
+    {
+        bool isAchive = false;
+
+        switch(achive)
+        {
+            case Achive.UnlockPotato:
+                isAchive = GameManager.instance.kill >= 10;  //게임매니저에 있는 kill이 10 이상이면 
+                break;
+            case Achive.UnlockBean:
+                isAchive = GameManager.instance.gameTime == GameManager.instance.maxGameTime;
+                break;
+
+        }
+
+        if (isAchive && PlayerPrefs.GetInt(achive.ToString()) == 0)
+        {
+            PlayerPrefs.SetInt(achive.ToString(), 1);
+
+            for(int index=0; index < uiNotice.transform.childCount; index++)
+            {
+
+                bool isActive = index == (int)achive; 
+                uiNotice.transform.GetChild(index).gameObject.SetActive(isActive);
+            }
+
+            StartCoroutine(NoticeRoutine());
+        }
+    }
+    IEnumerator NoticeRoutine()
+    {
+        uiNotice.SetActive(true);
+
+        yield return wait;   //4초뒤에 false로 변경
+
+        uiNotice.SetActive(false);
     }
 }
